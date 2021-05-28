e06cc9e4023bac77399a37b497bd2bd3be887a18
Ilias <igoujgali@gmail.com>
2021-05-28 23:59:04 +0200

    Ajout de l'adresse ip dans le html pour montrer le load balancing

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,13 +1,34 @@
 var express = require("express");
 var Chance = require("chance");
+const { networkInterfaces } = require("os");
 const chance = new Chance();
 var app = express();
 
+function getNetworkInfo() {
+  const nets = networkInterfaces();
+  const results = Object.create(null); // Or just '{}', an empty object
 
-function generatePosts(nbPostsRange = {min: 2,max: 10}, nbCommentsRange = {min: 2,max: 10}) {
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === "IPv4" && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+        results[name].push(net.address);
+      }
+    }
+  }
+  return results;
+}
+
+function generatePosts(
+  nbPostsRange = { min: 2, max: 10 },
+  nbCommentsRange = { min: 2, max: 10 }
+) {
   const posts = [];
-  const nbPosts = chance.integer(nbPostsRange)
- 
+  const nbPosts = chance.integer(nbPostsRange);
+
   // On génère des postes de blogs
   for (let i = 0; i < nbPosts; ++i) {
     const post = {
@@ -18,7 +39,7 @@ function generatePosts(nbPostsRange = {min: 2,max: 10}, nbCommentsRange = {min: 
       author: chance.name(),
       comments: [],
     };
-    const nbComments = chance.integer(nbCommentsRange)
+    const nbComments = chance.integer(nbCommentsRange);
     // On génère des commentaires pour ce post
     for (let j = 0; j < nbComments; ++j) {
       post.comments.push({
@@ -34,7 +55,7 @@ function generatePosts(nbPostsRange = {min: 2,max: 10}, nbCommentsRange = {min: 
 }
 // Get posts de blog
 app.get("/", function (req, res) {
-  res.send(generatePosts());
+  res.send({ data: generatePosts(), serverIp: getNetworkInfo().eth0[0] });
 });
 
 // On écoute sur le port 8080
